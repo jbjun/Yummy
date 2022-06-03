d91fd54087195bbdd3576ca3e6bfd22f2f10c329
오하영 <ohasis@ohasis-ui-MacBookAir.local>
2022-06-03 19:05:59 +0900

    메인화면 템플릿 세팅 및 연결

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import home.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',home.views.home, name='home'),
+    path('detail',home.views.detail,name='detail')
 ]
