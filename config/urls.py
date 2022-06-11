45fda5ecd887896d35d4492cf214e75d265d7c3e
iychoi <chin410@naver.com>
2022-06-11 21:25:58 +0900

    로그인 구현

--- config/urls.py
+++ config/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 import home.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',home.views.home, name='home'),
-    path('detail',home.views.detail,name='detail')
+    path('detail',home.views.detail,name='detail'),
+    path('auth/', include("member.urls"))
 ]
