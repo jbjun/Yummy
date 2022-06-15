3b086a9062edf845c1094452630231e43a7d7209
iychoi <chin410@naver.com>
2022-06-15 22:53:29 +0900

    회원가입 구현

--- member/views.py
+++ member/views.py
@@ -1,7 +1,7 @@
-import django
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect
+from .models import User
 
 # Create your views here.
 
@@ -25,3 +25,21 @@ def logout_view(request):
     logout(request)
 
     return redirect("member:login")
+
+def signup_view(request):
+
+    if request.method == "POST":
+        print(request.POST)
+        username = request.POST["username"]
+        password = request.POST["password"]
+        firstname = request.POST["firstname"]
+        lastname = request.POST["lastname"]
+        email = request.POST["email"]
+
+        user = User.objects.create_user(username,email,password)
+        user.first_name = firstname
+        user.last_name = lastname
+        user.save()
+        return redirect("member:login")
+
+    return render(request, "member/signup.html")
