5a5f1a20a6a9710ee254c58b47bfd36acca1d833
qudwns4351 <qudwns4351@naver.com>
2022-06-18 18:03:33 +0900

    사용자 게시판 퍼블...

--- board/views.py
+++ board/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def list(request):
+    return render(request,'list.html')
+
+def detail(request):
+    return render(request,'detail.html')
