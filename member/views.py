a4a26092e03a4c171bfe6cbdbbf8a344299e2516
iychoi <chin410@dahanit.co.kr>
2022-06-22 17:02:03 +0900

    회원가입 유효성검사

--- member/views.py
+++ member/views.py
@@ -1,7 +1,10 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect
 from .models import User
+from django.core.exceptions import ValidationError
+from .forms import SignupForm
 
 # Create your views here.
 
@@ -15,6 +18,7 @@ def login_view(request):
         if user is not None:
             print("인증성공")
             login(request,user)
+            return render(request, "index.html")
         else:
             print("인증실패")
         
@@ -26,20 +30,19 @@ def logout_view(request):
 
     return redirect("member:login")
 
+
 def signup_view(request):
+    if request.method == 'POST':
+        signup_form = SignupForm(request.POST)
+        # 유효성 검증에 통과한 경우 (username의 중복과 password1, 2의 일치 여부)
+        if signup_form.is_valid():
+            # SignupForm의 인스턴스 메서드인 signup() 실행, 유저 생성
+            signup_form.signup()
+            return redirect("member:login")
+    else:
+        signup_form = SignupForm()
 
-    if request.method == "POST":
-        print(request.POST)
-        username = request.POST["username"]
-        password = request.POST["password"]
-        firstname = request.POST["firstname"]
-        lastname = request.POST["lastname"]
-        email = request.POST["email"]
-
-        user = User.objects.create_user(username,email,password)
-        user.first_name = firstname
-        user.last_name = lastname
-        user.save()
-        return redirect("member:login")
-
-    return render(request, "member/signup.html")
+    context = {
+        'signup_form': signup_form,
+    }
+    return render(request, 'member/signup.html', context)
