632121dd222d78d19cdb2e14ec82d269ad0a32fd
iychoi <chin410@naver.com>
2022-06-26 22:24:52 +0900

    카카오 로긴

--- config/settings.py
+++ config/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.naver',
+    'allauth.socialaccount.providers.kakao',
 ]
 
 MIDDLEWARE = [
